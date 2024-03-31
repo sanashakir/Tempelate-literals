9bc948dfbff536b790213a5fde79c0eda48074f0
Sana Shakir <161624394+sanashakir@users.noreply.github.com>
2024-03-31 23:54:58 +0500

    Template literals

---
+++ Ttemplate literals/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//  template Literals:)
+let firstName = "Sana";
+let lastName = "Shakir";
+// back Ticks (`).
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+// SanaShakir
---
+++ Ttemplate literals/main.ts
@@ -0,0 +1,23 @@
+//  Template literals:)
+
+let firstName : string = "Sana";
+let lastName : string = "Shakir";
+
+// Back Ticks (`).
+let fullName : string = `${firstName} ${lastName}`;
+ 
+console.log(fullName);
+// Sana Shakir
+
+
+
+
+
+
+
+
+
+
+
+
+
